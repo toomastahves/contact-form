07e4692aa23f6a38f8151dfa770bae3b120c8e3d
Toomas Tahves <toomastahves@hotmail.com>
2016-04-12 19:18:55 +0300

    added favicon

--- server/koa/app.js
+++ server/koa/app.js
@@ -1,6 +1,7 @@
 import koa from 'koa';
 import cors from 'koa-cors';
 import serve from 'koa-static';
+import favicon from 'koa-favicon';
 import routes from '../routes/';
 
 import { connectToMainDatabase } from '../database/mongo';
@@ -11,6 +12,7 @@ app.use(cors());
 routes(app);
 
 app.use(serve('public'));
+app.use(favicon('public/favicon.png'));
 
 app.on('error', (err, ctx) => {
   console.log('server error', err, ctx);
